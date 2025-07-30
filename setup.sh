8528fab5558eea8269a6547cd706ebfdc7ebd9e5
Ray Seikel <rseikel@swin.edu.au>
2025-07-30 13:14:39 +1000

    only on ozstar

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [ "$SYS_ARCH" = "milan" ];then
 module purge &> /dev/null
 module load gcc/12.2.0
 module load boost/1.81.0
@@ -7,3 +8,4 @@ module load hdf5/1.14.0
 module load gcccore/12.2.0
 module load gsl/2.7
 module load cmake/3.24.3
+fi
