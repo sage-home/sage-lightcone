2be4e42cebf0ad9aa5e8250e048fec8cb0c8c602
einshoe <rseikel@swin.edu.au>
2026-02-04 14:09:30 +1100

    fix CI/CD build and add better test

--- tests/test_cli_validation.sh
+++ tests/test_cli_validation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source ./setup.sh
-# For now the validation tests still exit with 0 
+# For now the validation tests still exit with 0 (cli_lightcone doesn't return non-zero on validation errors yet)
+# These tests verify that invalid arguments are detected and reported.
+# Note: do NOT pass valid args here, as dummy.h5 is not a real KD-tree file
+# and cli_lightcone would attempt to process it.
 bin/cli_lightcone --dataset tests/dummy.h5 --decmin 100 --decmax 0
 bin/cli_lightcone --dataset tests/dummy.h5 --decmin 10 --decmax 0
 bin/cli_lightcone --dataset tests/dummy.h5 --decmin -10 --decmax 100
@@ -10,5 +13,3 @@ bin/cli_lightcone --dataset tests/dummy.h5 --ramin 10 --ramax 0
 bin/cli_lightcone --dataset tests/dummy.h5 --ramin -10 --ramax 400
 bin/cli_lightcone --dataset tests/dummy.h5 --ramin 0 --ramax 10 --zmin -1 --zmax 200
 bin/cli_lightcone --dataset tests/dummy.h5 --ramin 0 --ramax 10 --zmin 3 --zmax 2
-
-bin/cli_lightcone --dataset tests/dummy.h5 --decmin 0 --decmax 10 --ramin 0 --ramax 10 --outfields dummy
